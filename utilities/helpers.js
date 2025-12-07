60f9ba5499214359ffab0782f94cc8972684ca29
Zach Becker <zach@thebeckers.cc>
2025-12-07 15:48:49 -0600

    adding work in progress message

--- utilities/helpers.js
+++ utilities/helpers.js
@@ -120,7 +120,7 @@ function showNewPage()
     
     if (newPage === "about-me")
     {
-        document.getElementById('homePageTitle').textContent = 'Zach Becker\s Portfolio: About Me'
+        document.getElementById('homePageTitle').textContent = 'Zach Becker\s Portfolio: About Me (WORK IN PROGRESS)!'
         document.getElementById('homePageContent').style.display = "none";
         document.getElementById('aboutMePageContent').style.display = "grid";
         resizeCanvas();
