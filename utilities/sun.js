22e557505b16945bc80f5d2b5c01d031cdbcc7f5
Zach Becker <zach@thebeckers.cc>
2026-02-15 15:28:05 -0600

    spawning suns mnore evenly

--- utilities/sun.js
+++ utilities/sun.js
@@ -11,15 +11,19 @@ class Sun extends SpaceObject
 
         // keeps entirely within screen
         const bufferDist = sunConfig.radius.max + sunConfig.spread.randomness
-        for (let i = xMin + bufferDist; i < xMax - bufferDist; i += sunConfig.spread.distance)
-        {
-            const newSun = new Sun(
-                    i + getRandomFloat(-1*sunConfig.spread.randomness, sunConfig.spread.randomness), 
-                    getRandomFloat(yMin+sunConfig.radius.max, yMax-sunConfig.radius.max))
-            suns.push(newSun);
-            spawnedSuns.push(newSun)
-        }
 
+        // create suns in rows of 600 pixel height, randomly within the rows
+        for (let j = yMin; j + 600 < yMax; j += 600)
+        {
+            for (let i = xMin + bufferDist; i < xMax - bufferDist; i += sunConfig.spread.distance)
+            {
+                const newSun = new Sun(
+                        i + getRandomFloat(-1*sunConfig.spread.randomness, sunConfig.spread.randomness), 
+                        getRandomFloat(j+sunConfig.radius.max, j+600-sunConfig.radius.max))
+                suns.push(newSun);
+                spawnedSuns.push(newSun)
+            }
+        }
         return spawnedSuns;
     }
 }
